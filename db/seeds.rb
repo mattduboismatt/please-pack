0b6a397911394325839f32dcff936371ce53c42e
Matt DuBois <mattduboismatt@gmail.com>
2016-12-08 13:43:08 -0600

    add favicon and update seeds for Top Chef Season 14 picks

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,23 +4,31 @@ DatabaseCleaner.clean_with :truncation
 
 top_chef = create(:pool, title: "Top Chef Charleston - Season 14")
 
-create(:contestant, pool: top_chef, first_name: "Annie", last_name: "Pettry", residence: "Louisville, KY", description: "Exec Chef / Owner, Decca")
-create(:contestant, pool: top_chef, first_name: "BJ", last_name: "Smith", residence: "Portland, OR", description: "Exec Chef / Owner, Smokehouse 21")
-create(:contestant, pool: top_chef, first_name: "Emily", last_name: "Hahn", residence: "Charleston, SC", description: "Chef, Warehouse Bar + Kitchen")
-create(:contestant, pool: top_chef, first_name: "Gerlad", last_name: "Sombright", residence: "Marco Island, FL", description: "Chef de Cuisine, Ario")
-create(:contestant, pool: top_chef, first_name: "Jamie", last_name: "Lynch", residence: "Charlotte, NC", description: "Exec Chef / Partner, 5Church Group")
-create(:contestant, pool: top_chef, first_name: "Jim", last_name: "Smith", residence: "Montgomery, AL", description: "Exec Chef, State of Alabama")
-create(:contestant, pool: top_chef, first_name: "Silvia", last_name: "Barban", residence: "Brooklyn, NY", description: "Chef / Owner, Aita and LaRina")
-create(:contestant, pool: top_chef, first_name: "Sylva", last_name: "Senat", residence: "Philadelphia, PA", description: "Exec Chef, Maison208")
-create(:contestant, pool: top_chef, first_name: "Amanda", last_name: "Baumgarten", residence: "Chicago, IL", description: "Bartender, The Victor Bar")
-create(:contestant, pool: top_chef, first_name: "Brooke", last_name: "Williamson", residence: "Los Angeles, CA", description: "Exec Chef / Owner, Playa Provisions")
-create(:contestant, pool: top_chef, first_name: "Casey", last_name: "Thompson", residence: "Napa Valley, CA", description: "Exec Chef, Inn at Rancho Santa Fe")
-create(:contestant, pool: top_chef, first_name: "John", last_name: "Tesar", residence: "Dallas, TX", description: "Chef / Partner, Knife at the Highland")
-create(:contestant, pool: top_chef, first_name: "Katsuji", last_name: "Tanabe", residence: "Los Angeles, CA", description: "Exec Chef / Owner, Mexikosher")
-create(:contestant, pool: top_chef, first_name: "Sam", last_name: "Talbot", residence: "Brooklyn, NY", description: "Exef Chef / Owner, Pretty Southern")
-create(:contestant, pool: top_chef, first_name: "Sheldon", last_name: "Simeon", residence: "Lahaina, HI", description: "Exef Chef / Owner, Tin Roof")
-create(:contestant, pool: top_chef, first_name: "Shirley", last_name: "Chung", residence: "Orange County, CA", description: "Chef / Partner, Twenty Eight")
+annie = create(:contestant, pool: top_chef, first_name: "Annie", last_name: "Pettry", residence: "Louisville, KY", description: "Exec Chef / Owner, Decca")
+bj = create(:contestant, pool: top_chef, first_name: "BJ", last_name: "Smith", residence: "Portland, OR", description: "Exec Chef / Owner, Smokehouse 21")
+emily = create(:contestant, pool: top_chef, first_name: "Emily", last_name: "Hahn", residence: "Charleston, SC", description: "Chef, Warehouse Bar + Kitchen")
+gerald = create(:contestant, pool: top_chef, first_name: "Gerlad", last_name: "Sombright", residence: "Marco Island, FL", description: "Chef de Cuisine, Ario")
+jamie = create(:contestant, pool: top_chef, first_name: "Jamie", last_name: "Lynch", residence: "Charlotte, NC", description: "Exec Chef / Partner, 5Church Group")
+jim = create(:contestant, pool: top_chef, first_name: "Jim", last_name: "Smith", residence: "Montgomery, AL", description: "Exec Chef, State of Alabama")
+silvia = create(:contestant, pool: top_chef, first_name: "Silvia", last_name: "Barban", residence: "Brooklyn, NY", description: "Chef / Owner, Aita and LaRina")
+sylva = create(:contestant, pool: top_chef, first_name: "Sylva", last_name: "Senat", residence: "Philadelphia, PA", description: "Exec Chef, Maison208")
+amanada = create(:contestant, pool: top_chef, first_name: "Amanda", last_name: "Baumgarten", residence: "Chicago, IL", description: "Bartender, The Victor Bar")
+brooke = create(:contestant, pool: top_chef, first_name: "Brooke", last_name: "Williamson", residence: "Los Angeles, CA", description: "Exec Chef / Owner, Playa Provisions")
+casey = create(:contestant, pool: top_chef, first_name: "Casey", last_name: "Thompson", residence: "Napa Valley, CA", description: "Exec Chef, Inn at Rancho Santa Fe")
+john = create(:contestant, pool: top_chef, first_name: "John", last_name: "Tesar", residence: "Dallas, TX", description: "Chef / Partner, Knife at the Highland")
+katsuji = create(:contestant, pool: top_chef, first_name: "Katsuji", last_name: "Tanabe", residence: "Los Angeles, CA", description: "Exec Chef / Owner, Mexikosher")
+sam = create(:contestant, pool: top_chef, first_name: "Sam", last_name: "Talbot", residence: "Brooklyn, NY", description: "Exef Chef / Owner, Pretty Southern")
+sheldon = create(:contestant, pool: top_chef, first_name: "Sheldon", last_name: "Simeon", residence: "Lahaina, HI", description: "Exef Chef / Owner, Tin Roof")
+shirley = create(:contestant, pool: top_chef, first_name: "Shirley", last_name: "Chung", residence: "Orange County, CA", description: "Chef / Partner, Twenty Eight")
 
-%w(Steve Tom Jason Courtney Matt).each do |name|
-  create(:entry, pool: top_chef, name: name, points: 0)
-end
+matt = create(:entry, pool: top_chef, name: "Matt", points: 0)
+tom = create(:entry, pool: top_chef, name: "Tom", points: 0)
+jason = create(:entry, pool: top_chef, name: "Jason", points: 0)
+courtney = create(:entry, pool: top_chef, name: "Courtney", points: 0)
+steve = create(:entry, pool: top_chef, name: "Steve", points: 0)
+
+[sheldon, shirley, brooke, emily, john, jamie].each { |contestant| create(:pick, entry: matt, contestant: contestant) }
+[sam, silvia, shirley, brooke, sylva, jamie].each { |contestant| create(:pick, entry: steve, contestant: contestant) }
+[jamie, sam, sheldon, shirley, silvia, brooke].each { |contestant| create(:pick, entry: tom, contestant: contestant) }
+[brooke, casey, jim, jamie, john, annie].each { |contestant| create(:pick, entry: jason, contestant: contestant) }
+[silvia, jim, sam, shirley, sylva, brooke].each { |contestant| create(:pick, entry: courtney, contestant: contestant) }
