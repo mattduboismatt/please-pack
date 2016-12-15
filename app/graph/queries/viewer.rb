8119537f61f331c97f142e66a1142f1e5879b209
Matt DuBois <mattduboismatt@gmail.com>
2016-12-15 13:17:48 -0600

    update queries to ensure using DB models, not other query objects

--- app/graph/queries/viewer.rb
+++ app/graph/queries/viewer.rb
@@ -16,25 +16,25 @@ module Queries
     field :entry do
       type !EntryType
       argument :model_id, !types.ID, "The ID of the Entry"
-      resolve ->(_object, args, _context) { Entry.find(args["model_id"]) }
+      resolve ->(_object, args, _context) { ::Entry.find(args["model_id"]) }
     end
 
     connection :pools, PoolType.connection_type do
       description "All pools in order of id"
 
-      resolve ->(_object, _args, _context) { Pool.all }
+      resolve ->(_object, _args, _context) { ::Pool.all }
     end
 
     field :score_mechanisms do
       type !types[types.String]
       description "Options for the score mechanism"
-      resolve ->(_object, _args, _context) { Score::MECHANISMS.all }
+      resolve ->(_object, _args, _context) { ::Score::MECHANISMS.all }
     end
 
     connection :scores, ScoreType.connection_type do
       description "All scores in order of id"
 
-      resolve ->(_object, _args, _context) { Score.all }
+      resolve ->(_object, _args, _context) { ::Score.all }
     end
   end
 end
